f66a4a3607a6c48a0b9b7004c1f923762c702aa5
Steven Renaux <steven.renaux@sensiolabs.com>
2024-07-06 14:35:51 +0200

    project setup

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Faker\Factory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/invoice', name: 'invoice_')]
+class InvoiceController extends AbstractController
+{
+    private function invoiceData(): array
+    {
+        $factory = Factory::create();
+
+        $allPurchases = [];
+        for ($i = 0; $i < 20; $i++) {
+            $allPurchases[] = [
+                'orderId' => $factory->uuid(),
+                'period' => $factory->dateTimeBetween('- 1 week')->format('Y-m-d') . ' ' . $factory->dateTime('now')->format('Y-m-d'),
+                'description' => $factory->sentence(),
+                'price' => $factory->randomFloat(2, 1),
+                'quantity' => $factory->randomDigitNotZero(),
+                'total' => $factory->randomFloat(2, 1),
+            ];
+        }
+
+        return [
+            'invoice' => [
+                'id' => $factory->unixTime(),
+                'date' => $factory->dateTime()->format('Y-m-d'),
+                'due_date' => $factory->dateTime('+1 week')->format('Y-m-d'),
+                'sub_total' => $factory->randomFloat(2, 1),
+                'total' => $factory->randomFloat(2, 1),
+            ],
+            'client' => [
+                'name' => $factory->company(),
+                'address' => $factory->address(),
+                'city' => $factory->city(),
+            ],
+            'purchases' => $allPurchases
+        ];
+    }
+}
