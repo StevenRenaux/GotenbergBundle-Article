e6391cf28a126c7fcc9a3f5dc628f35939943677
Steven Renaux <steven.renaux@sensiolabs.com>
2024-07-06 14:45:24 +0200

    install GotenbergBundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Sensiolabs\GotenbergBundle\SensiolabsGotenbergBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
